4276dcda00b7d4faf4ed9ed56bd8da473a2fe0ee
Luis Ascencio <luiscarlosasqu@ufps.edu.co>
2022-10-20 18:00:45 -0500

    Cambiando la estructura del proyecto

---
+++ src/main/java/com/fogatta/FogattaRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.fogatta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FogattaRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FogattaRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fogatta/FogattaRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fogatta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FogattaRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
