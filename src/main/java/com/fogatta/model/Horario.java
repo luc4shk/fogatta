3ef799417d4a4fed98469c722895296ae2bd0b0e
Samuel Boada <samuelomarboba@ufps.edu.co>
2022-11-07 16:12:37 -0500

    se terminó la clase mesa y se realizó la clase horario

---
+++ src/main/java/com/fogatta/model/Horario.java
@@ -0,0 +1,59 @@
+
+package com.fogatta.model;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author omars
+ */
+public class Horario {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    
+    @Column(nullable = false, length = 7)
+    private String hora;
+    
+    private boolean estado;
+
+    public Horario() {
+    }
+
+    public Horario(Integer id, String hora, boolean estado) {
+        this.id = id;
+        this.hora = hora;
+        this.estado = estado;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getHora() {
+        return hora;
+    }
+
+    public void setHora(String hora) {
+        this.hora = hora;
+    }
+
+    public boolean isEstado() {
+        return estado;
+    }
+
+    public void setEstado(boolean estado) {
+        this.estado = estado;
+    }
+    
+    
+    
+}
--- src/main/java/com/fogatta/model/Mesa.java
+++ src/main/java/com/fogatta/model/Mesa.java
@@ -1,14 +1,17 @@
 package com.fogatta.model;
 
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -23,9 +26,57 @@ public class Mesa {
     private Integer max_ocupantes;
             
     @Column(nullable = false)
-    private Character ocupada;
+    private boolean ocupada;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+        name = "mesa_horario", 
+        joinColumns = @JoinColumn(name = "mesa_id"),
+        inverseJoinColumns = @JoinColumn(name = "horario_id")
+    )
+    private Set<Horario> productos = new HashSet<>();
+
+    public Mesa() {
+    }
+
+    public Mesa(Integer id, Integer max_ocupantes, boolean ocupada) {
+        this.id = id;
+        this.max_ocupantes = max_ocupantes;
+        this.ocupada = ocupada;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getMax_ocupantes() {
+        return max_ocupantes;
+    }
+
+    public void setMax_ocupantes(Integer max_ocupantes) {
+        this.max_ocupantes = max_ocupantes;
+    }
+
+    public boolean getOcupada() {
+        return ocupada;
+    }
+
+    public void setOcupada(boolean ocupada) {
+        this.ocupada = ocupada;
+    }
+
+    public Set<Horario> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(Set<Horario> productos) {
+        this.productos = productos;
+    }
     
-    /*Constructor*/
     
     
 }
