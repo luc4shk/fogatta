519ffab1320efddf3ca31f95e0317104d07ca4f0
Samuel Boada <samuelomarboba@ufps.edu.co>
2022-11-14 11:16:25 -0500

    Agregando alerta eliminar

--- src/main/resources/static/js/alertIndex.js
+++ src/main/resources/static/js/alertIndex.js
@@ -1,6 +1,7 @@
 
 const boton = document.getElementById('boton');
 const formulario = document.getElementById('formulario');
+const btnEliminar = document.getElementById('btnEliminar');
 
 boton.addEventListener("click", (e) => {
 
@@ -29,4 +30,39 @@ boton.addEventListener("click", (e) => {
             }
         })
     }
+})
+
+const swalWithBootstrapButtons = Swal.mixin({
+    customClass: {
+        confirmButton: 'btn btn-success',
+        cancelButton: 'btn btn-danger'
+    },
+    buttonsStyling: false
+})
+
+swalWithBootstrapButtons.fire({
+    title: 'Are you sure?',
+    text: "You won't be able to revert this!",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonText: 'Yes, delete it!',
+    cancelButtonText: 'No, cancel!',
+    reverseButtons: true
+}).then((result) => {
+    if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire(
+        'Deleted!',
+        'Your file has been deleted.',
+        'success'
+    )
+  } else if (
+    /* Read more about handling dismissals below */
+        result.dismiss === Swal.DismissReason.cancel
+  ) {
+    swalWithBootstrapButtons.fire(
+        'Cancelled',
+        'Your imaginary file is safe :)',
+        'error'
+    )
+  }
 })
