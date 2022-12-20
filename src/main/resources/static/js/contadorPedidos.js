dc3aeb4297cf81c0a6e2a02c62b460c9ec5b00e0
Luis Ascencio <luiscarlosasqu@ufps.edu.co>
2022-12-20 12:53:32 -0500

    Modificando estilos del formulario de pedidos

--- src/main/resources/static/js/contadorPedidos.js
+++ src/main/resources/static/js/contadorPedidos.js
@@ -1,19 +1,38 @@
-const menos = document.getElementById("menos")
-const mas = document.getElementById("mas")
-const contador = document.getElementById("numeroProducto")
+const menos = document.getElementById("menos");
+const mas = document.getElementById("mas");
+const contador = document.getElementById("numeroProducto");
+const bebida = document.querySelector("#bebida");
+const comida = document.querySelector("#comida");
+const precio = document.getElementById("precio");
 
-let numero=parseFloat(contador.innerHTML);
+let numero = parseFloat(contador.innerHTML);
+let bebidaSeleccionada;
+let comidaSeleccionada;
 
-menos.addEventListener("click", ()=>{
-    if(numero>1){
-    numero=numero-1
-    contador.innerHTML=numero
-    }
-})
+const actualizarPrecio = () => {
+  bebidaSeleccionada = bebida.options[bebida.selectedIndex].value;
+  comidaSeleccionada = comida.options[comida.selectedIndex].value;
+  const res = (parseFloat(bebidaSeleccionada) + parseFloat(comidaSeleccionada)) * numero;
+  precio.innerHTML = `$${res}`;
+};
 
-mas.addEventListener("click", ()=>{
-    numero=numero+1
-    contador.innerHTML=numero
-    console.log("hola")
+window.addEventListener("load", () => {
+  actualizarPrecio();
+});
 
-})
+bebida.addEventListener("change", actualizarPrecio);
+comida.addEventListener("change", actualizarPrecio);
+
+menos.addEventListener("click", () => {
+  if (numero > 1) {
+    numero -= 1;
+    contador.innerHTML = numero;
+    actualizarPrecio();
+  }
+});
+
+mas.addEventListener("click", () => {
+  numero += 1;
+  contador.innerHTML = numero;
+  actualizarPrecio();
+});
